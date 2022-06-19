3b48050401dbed3f5a83e6bda912f952fa09c4c2
shahedkh <alkhalailah.shahed@gmail.com>
2022-06-20 00:38:42 +0300

    add todo item and data

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import './App.css';
+import Todos from './components/Todos';
+import Todo from './models/todo'
 
 function App() {
+  const todos = [
+    new Todo('learn react'),
+    new Todo('learn typeScript')
+  ]
   return (
     <div>
+      <Todos items={todos}/>
     </div>
   );
 }
-
 export default App;
---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,4 @@
+const TodoItem: React.FC <{text : string}>= (props) => {
+return <li>{props.text}</li>
+}
+export default TodoItem
