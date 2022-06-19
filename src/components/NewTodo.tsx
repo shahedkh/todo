15df56aae30c61ce8d89d717d5ad53c86e315c47
shahedkh <alkhalailah.shahed@gmail.com>
2022-06-20 00:46:27 +0300

    new todo component

---
+++ src/components/NewTodo.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const NewTodo : React.FC = (props) => {
+const submitHandler = (event : React.FormEvent)=>{
+event.preventDefault()
+}
+
+return <form onSubmit={submitHandler}>
+    <label htmlFor="text">Todo Text</label>
+    <input type='text' id="text"/>
+    <button>Add Todo</button>
+</form>
+}
+export default NewTodo;
